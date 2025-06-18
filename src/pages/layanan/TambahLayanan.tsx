c756ec25857eb7524240df2f57ec6b8986220bdd
wirawanmahardika <wiraprogrammer@gmail.com>
2025-06-18 20:29:46 +0800

    feat: integrasi ke backend pada halaman tambah layanan

--- src/pages/layanan/TambahLayanan.tsx
+++ src/pages/layanan/TambahLayanan.tsx
@@ -1,22 +1,50 @@
+import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
+import { AxiosAuth } from "../../utils/axios";
+import { useNavigate } from "react-router-dom";
 
 export default function TambahLayanan() {
+    const navigate = useNavigate()
     useAuth()
+    const tambahLayanan = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const body = {
+            nama: e.currentTarget.nama.value,
+            harga: parseInt(e.currentTarget.harga.value) ?? 0,
+            satuan: e.currentTarget.satuan.value
+        }
+        
+        try {
+            const res = await AxiosAuth.post("/layanan", body)
+            await Swal.fire({
+                text: res.data.message,
+                icon: "success",
+            })
+            navigate('/layanan')
+        } catch (error: any) {
+            Swal.fire({
+                text: error.response.data.message,
+                icon: "error",
+            })
+        }
+        
+    }
+
     return (
         <div className="flex items-center justify-center min-h-full">
-            <form className="bg-base-100 rounded-xl shadow-lg p-8 flex flex-col items-center gap-y-4 w-full max-w-md border border-base-300">
+            <form onSubmit={tambahLayanan} className="bg-base-100 rounded-xl shadow-lg p-8 flex flex-col items-center gap-y-4 w-full max-w-md border border-base-300">
                 <h2 className="font-bold text-2xl mb-2 text-sky-700">Tambah Layanan</h2>
                 <div className="flex flex-col gap-y-1 w-full">
                     <label className="font-semibold text-slate-600">Nama</label>
-                    <input type="text" className="input input-bordered w-full" placeholder="Masukkan nama..." required />
+                    <input type="text" name="nama" className="input input-bordered w-full" placeholder="Masukkan nama..." required />
                 </div>
                 <div className="flex flex-col gap-y-1 w-full">
                     <label className="font-semibold text-slate-600">Satuan</label>
-                    <input type="number" className="input input-bordered w-full" placeholder="Masukkan satuan..." required />
+                    <input type="text" name="satuan" className="input input-bordered w-full" placeholder="Masukkan satuan..." required />
                 </div>
                 <div className="flex flex-col gap-y-1 w-full">
                     <label className="font-semibold text-slate-600">Harga</label>
-                    <input type="number" className="input input-bordered w-full" placeholder="Masukkan harga..." required />
+                    <input type="number" name="harga" className="input input-bordered w-full" placeholder="Masukkan harga..." required />
                 </div>
                 <button className="btn btn-primary btn-md mt-4 w-full shadow">Tambah</button>
             </form>
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,3 +4,8 @@ export const AxiosAuth = axios.create({
     withCredentials: true,
     baseURL: import.meta.env.VITE_BASE_URL
 })
+
+AxiosAuth.interceptors.request.use(async (config) => {
+    config.headers.Authorization = "Bearer " + localStorage.getItem("token")
+    return config
+})
